693473f685a89c5341988d34382861eba04c3cb7
AndreiaRizzo <161900122+AndreiaRizzo@users.noreply.github.com>
2024-08-19 11:35:57 -0300

    at

--- empresarios/views.py
+++ empresarios/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cadastrar_empresa(request):
+    if request.method == "GET":
+        return render(request, 'cadastrar_empresa.html')
